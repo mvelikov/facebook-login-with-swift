1b2b9166254739d8b68a7f8eb837385418f3f266
Mihail Velikov <mihailvelik@gmail.com>
2014-10-30 16:50:58 +0200

    Create Bridge-Header class and add it to project

---
+++ Login with Swift iOS App/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  Login with Swift iOS App
+//
+//  Created by Mihail Velikov on 10/30/14.
+//  Copyright (c) 2014 Mihail Velikov. All rights reserved.
+//
+
+#ifndef Login_with_Swift_iOS_App_Bridge_Header_h
+#define Login_with_Swift_iOS_App_Bridge_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
