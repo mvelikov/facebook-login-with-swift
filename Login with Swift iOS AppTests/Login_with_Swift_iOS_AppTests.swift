8fec91895446bc09d6f03a8729c583a6a94c55f8
Mihail Velikov <mihailvelik@gmail.com>
2014-10-30 16:08:47 +0200

    Initial commit

---
+++ Login with Swift iOS AppTests/Login_with_Swift_iOS_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Login_with_Swift_iOS_AppTests.swift
+//  Login with Swift iOS AppTests
+//
+//  Created by Mihail Velikov on 10/30/14.
+//  Copyright (c) 2014 Mihail Velikov. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Login_with_Swift_iOS_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
