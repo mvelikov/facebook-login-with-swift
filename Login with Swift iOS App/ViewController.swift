8be19ec917a919ab6f233cbb9a1ff8889b1bf8df
Mihail Velikov <mihailvelik@gmail.com>
2014-10-30 17:05:25 +0200

    Add Facebook Delegate class with delegate method and FacebookLogin instance

--- Login with Swift iOS App/ViewController.swift
+++ Login with Swift iOS App/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBLoginViewDelegate {
 
+    @IBOutlet var fbLoginView : FBLoginView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    // Facebook Delegate Methods
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+        println("This is where you perform a segue.")
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User Name: \(user.name)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
     }
 
     override func didReceiveMemoryWarning() {
