eb6f723d43550e7bbb0df6ff838f412d289d12ea
Mihail Velikov <mihailvelik@gmail.com>
2014-10-31 11:06:35 +0200

    Downgrade project to baiscs

--- Login with Swift iOS App/ViewController.swift
+++ Login with Swift iOS App/ViewController.swift
@@ -8,35 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, FBLoginViewDelegate {
+class ViewController: UIViewController {
 
-    @IBOutlet var fbLoginView : FBLoginView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
 
-        self.fbLoginView.delegate = self
-        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
-    }
-    
-    // Facebook Delegate Methods
-    
-    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
-        println("User Logged In")
-        println("This is where you perform a segue.")
-    }
-    
-    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
-        println("User Name: \(user.name)")
-    }
-    
-    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
-        println("User Logged Out")
-    }
-    
-    func loginView(loginView : FBLoginView!, handleError:NSError) {
-        println("Error: \(handleError.localizedDescription)")
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
